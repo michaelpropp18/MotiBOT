05225bb822a6d32947d0ad47f027d66ed7ea258b
Otis Smith <themroats@gmail.com>
2021-04-10 15:30:07 -0400

    something

--- MotiBOT.py
+++ MotiBOT.py
@@ -1,1 +1,12 @@
 print("hello world")
+
+import requests
+import json
+
+
+x = requests.get('http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en')
+j = x.json()
+msg = []
+print('Motivational quote of the day: "' + j['quoteText'] + '" -' + j["quoteAuthor"])
+
+
