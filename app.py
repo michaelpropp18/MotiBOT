d5f075351206c7905a85b5af9b07a76454e7ec05
Otis Smith <themroats@gmail.com>
2021-04-10 15:56:37 -0400

    update flask

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 import os
 from flask import Flask
+from requests.api import request
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def hello():
     return "Hello World!"
 
 
-@app.route('/<name>')
+@app.route('/callback', methods = ['POST'])
 def hello_name(name):
+    print(request)
     return "Hello {}!".format(name)
 
 if __name__ == '__main__':
